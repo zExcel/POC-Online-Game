b1ba3ac70cec1339cab7c28f6824f00f5a3d0374
N0XIRE <connorwilson989@gmail.com>
2020-03-30 18:45:21 -0500

    Final Island2 Changes

--- Assets/MENUS/MainMenu.cs
+++ Assets/MENUS/MainMenu.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour{
+public class MainMenu : MonoBehaviour {
+
+    void Start ()
+    {
+        print("test");
+    }
 
     public void PlayGame ()
     {
-        SceneManager.LoadScene("Island");
+        string sceneName = "Island";
+        
+        SceneManager.LoadSceneAsync(sceneName);
     }
 
 }
